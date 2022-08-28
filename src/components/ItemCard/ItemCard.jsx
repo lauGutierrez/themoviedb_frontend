0eb4d2ee5344b349f4f8a8c4b1fa03ff0800f134
Laura Gutiérrez <laura.gutierrez.lopezfranca@gmail.com>
2022-08-28 18:19:58 +0200

    chore: adding side menu to show card details (wip)

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,72 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './ItemCard.scss';
 
 import { IMAGES_URL } from '../../const/moviesApi';
 
-import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import CardContent from '@mui/material/CardContent';
+import Collapse from '@mui/material/Collapse';
+import Typography from '@mui/material/Typography';
+import Drawer from '@mui/material/Drawer';
 
 
 const ItemCard = (props) => {
-  const [fullCard, setFullCard] = useState(false);
-  const [isOverviewVisible, setIsOverviewVisible] = useState(false);
+  const [isVisibleOverview, setIsVisibleOverview] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
 
-  const showFullCard = () => {
-    setFullCard(true);
-  }
-
-  const hideFullCard = () => {
-    setFullCard(false);
-  }
+  useEffect(() => {
+    setIsVisibleOverview(isOpened);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpened]);
 
   return (
-    <Card className="item-card"
-      onMouseEnter={(event) => showFullCard()}
-      onMouseLeave={(event) => hideFullCard()}
-      onClick={() => setIsOverviewVisible(!isOverviewVisible)}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="400"
-          image={props.image ? IMAGES_URL + props.image : "/images/no-image-available.jpg"}/>
-        {fullCard ? 
-          (
-            <React.Fragment>
+    <React.Fragment>
+      <Card className="item-card" onClick={() => setIsOpened(true)}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="400"
+            image={props.image ? IMAGES_URL + props.image : "/images/no-image-available.jpg"} />
+        </CardActionArea>
+      </Card>
+      <Drawer
+        anchor="right"
+        open={isOpened}
+        onClick={() => setIsOpened(false)}>
+        <Card className="item-card-detail">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="400"
+              image={props.image ? IMAGES_URL + props.image : "/images/no-image-available.jpg"} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                {props.title}
+              </Typography>
+            </CardContent>
+            <Collapse in={isVisibleOverview} timeout="auto" unmountOnExit>
               <CardContent>
-                <Grid container
-                  direction="row"
-                  justifyContent="flex-end">
-                  <Grid item xs={8} sm={8} md={10} lg={11} xl={11}>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {props.title}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={4} sm={4} md={2} lg={1} xl={1} className="expand-more">
-                    <ExpandMoreIcon
-                      className={isOverviewVisible ? "rotate" : ""}/>
-                  </Grid>
-                </Grid>
+                <Typography paragraph>{props.overview}</Typography>
               </CardContent>
-              <Collapse in={isOverviewVisible} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography paragraph>{props.overview}</Typography>
-                </CardContent>
-              </Collapse>
-            </React.Fragment>
-          ) :
-          null
-        }
-        
-      </CardActionArea>
-    </Card>
+            </Collapse>
+          </CardActionArea>
+        </Card>
+      </Drawer>
+    </React.Fragment>
   );
 }
 
